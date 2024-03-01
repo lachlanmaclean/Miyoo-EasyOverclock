bbdb368d88393cf64fd35c7050e28b61cc69cc60
Lachlan Maclean <lachlanmaclean97@gmail.com>
2024-03-01 18:40:56 -0500

    Init repo

---
+++ launch.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd /mnt/SDCARD/.tmp_update
+HOME=/mnt/SDCARD ./bin/st -q -e /mnt/SDCARD/App/EasyOverclock/main.sh
---
+++ main.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#Find the device model 354 is MMP, 283 is MM
+
+MODEL=$(<"/tmp/deviceModel")
+echo $MODEL
+
+#echo "Hello, world!" 
+
+#read -p "Press start to exit"
